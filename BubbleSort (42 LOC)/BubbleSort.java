674441b775b53f3c89e993649a3ddee9d5d675f2
141638 <55794812+141638@users.noreply.github.com>
2019-11-21 11:22:26 +0700

    Update BubbleSort.java

--- BubbleSort (42 LOC)/BubbleSort.java
+++ BubbleSort (42 LOC)/BubbleSort.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  *
- * @author admin
+ * @author 141638
  */
 public class BubbleSort {
     
