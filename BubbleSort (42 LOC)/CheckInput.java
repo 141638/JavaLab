98d9de8a9b8a92bbc3dd0ca62c2a01047964f79a
141638 <55794812+141638@users.noreply.github.com>
2019-11-21 11:22:54 +0700

    Update CheckInput.java

--- BubbleSort (42 LOC)/CheckInput.java
+++ BubbleSort (42 LOC)/CheckInput.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 import java.util.Scanner;
 
 /**
  *
- * @author admin
+ * @author 141638
  */
 public class CheckInput {
     Scanner sc = new Scanner(System.in);
