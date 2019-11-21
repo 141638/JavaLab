e26cb3623218e7dfc4b5d9575fe4ebf51f30852d
141638 <55794812+141638@users.noreply.github.com>
2019-11-21 11:02:46 +0700

    Update main.java

--- Fibonacci (50 LOC)/main.java
+++ Fibonacci (50 LOC)/main.java
@@ -1,1 +1,19 @@
 
+public class main {
+
+    static int fib(int n) { 
+        if (n ==0 || n == 1) 
+           return n; 
+        return fib(n-1) + fib(n-2); 
+    } 
+    
+    public static void main(String[] args) {
+        System.out.println("The 45 sequence of Fibonacci: ");
+        for(int i = 0;i<45;i++){
+            if(i%2==0)
+                System.out.println("");
+            System.out.printf(" [%d] ", fib(i));
+        }    
+        System.out.println("");
+    }
+}
